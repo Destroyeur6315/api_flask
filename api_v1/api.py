7980a89c00ab17c3fefa8458a3378dd932da8d9f
Destroyeur6315 <romain.fillot630@gmail.com>
2024-02-25 11:17:01 +0100

    :sparkles: endpoint done (auth good for one endpoint)

--- api_v1/api.py
+++ api_v1/api.py
@@ -8,6 +8,7 @@ app.config["DEBUG"] = True
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+fake_API_KEY = "kajpazn494JEJDkajbd24kjbzbdhv344"
 
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
@@ -76,7 +77,7 @@ def add_Items():
     
 
 @app.route('/api/v1/resources/items/<int:id_article>', methods=['GET', 'PUT', 'DELETE'])
-def get_Items(id_article):
+def handle_Items_By_Id(id_article):
     conn = sqlite3.connect('database.db')
 
     # Return Object row which seems dict (tuple by default)
@@ -105,17 +106,174 @@ def get_Items(id_article):
         conn.commit()
         conn.close()
 
-        return jsonify({'success': True, 'message': "items update"}), 200
+        return jsonify({'success': True, 'message': "item update"}), 200
     elif request.method == "DELETE":
         item = cur.execute('DELETE FROM article WHERE id_article=?',[id_article])
 
         conn.commit()
         conn.close()
-        return jsonify({'success': True, 'message': "items delete"}), 200
+        return jsonify({'success': True, 'message': "item delete"}), 200
     else :
         conn.close()
         return jsonify({'success': False, 'message': 'method used unknown'}), 404
     
+
+@app.route('/api/v1/resources/files', methods=['GET'])
+def get_All_Files():
+    conn = sqlite3.connect('database.db')
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    items = cur.execute('SELECT * FROM dossier;').fetchall()
+
+    # Convert object Row () into dict python
+    items_dict = [dict(row) for row in items]
+
+    return jsonify(items_dict)
+
+
+@app.route('/api/v1/resources/files/<int:id_dossier>', methods=['GET'])
+def get_Files_By_Id(id_dossier):
+    conn = sqlite3.connect('database.db')
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM dossier WHERE id_dossier=?',[id_dossier]).fetchall()
+
+    # Convert object Row () into dict python
+    items_dict = [dict(row) for row in item]
+
+    return jsonify(items_dict)
+
+
+@app.route('/api/v1/resources/files/<int:id_dossier>', methods=['DELETE'])
+def delete_Files_By_Id(id_dossier):
+    conn = sqlite3.connect('database.db')
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM dossier WHERE id_dossier=?',[id_dossier]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'file not found'}), 404
+
+    item = cur.execute('DELETE FROM dossier WHERE id_dossier=?',[id_dossier])
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({'success': True, 'message': "file delete"}), 200
+
+
+@app.route('/api/v1/resources/files/<int:id_dossier>', methods=['PUT'])
+def update_Files_By_Id(id_dossier):
+
+    auth_header = request.headers.get('X-API-KEY')
+
+    if auth_header != fake_API_KEY:
+        return jsonify({'success': False, 'message': 'API key invalid'}), 401
+
+    conn = sqlite3.connect('database.db')
+
+    id_article = request.json['id_article']
+    num_dossier = request.json['num_dossier']
+    date = request.json['date']
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM dossier WHERE id_dossier=?',[id_dossier]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'file not found'}), 404
+
+    item = cur.execute('UPDATE dossier SET num_dossier=?, \
+            date=?, id_article=? WHERE id_dossier=?',[num_dossier, date, id_article, id_dossier])
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({'success': True, 'message': "file update"}), 200
+
+
+@app.route('/api/v1/resources/items/<int:id_article>/files', methods=['GET'])
+def get_Files_By_ItemId(id_article):
+    conn = sqlite3.connect('database.db')
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM dossier WHERE id_article=?',[id_article]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'item not found'}), 404
+
+    # Convert object Row () into dict python
+    items_dict = [dict(row) for row in item]
+
+    return jsonify(items_dict)
+
+
+@app.route('/api/v1/resources/items/<int:id_article>/files', methods=['POST'])
+def add_Files_By_ItemId(id_article):
+    conn = sqlite3.connect('database.db')
+
+    id = request.json['id_dossier']
+    num_dossier = request.json['num_dossier']
+    date = request.json['date']
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM article WHERE id_article=?',[id_article]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'item not found'}), 404
+
+    item = cur.execute('INSERT INTO dossier \
+            (id_dossier, num_dossier, date, id_article) VALUES (?, ?, ?, ?)', 
+                (id, num_dossier, date, id_article))
+
+    conn.commit()
+    conn.close()
+    return jsonify({'success': True, 'message': "New file created"}), 200
+
+
+@app.route('/api/v1/resources/items/<int:id_article>/files/<int:id_dossier>', methods=['PUT'])
+def update_Files_By_ItemId(id_article, id_dossier):
+    conn = sqlite3.connect('database.db')
+
+    num_dossier = request.json['num_dossier']
+    date = request.json['date']
+
+    # Return Object row which seems dict (tuple by default)
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    item = cur.execute('SELECT * FROM article WHERE id_article=?',[id_article]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'item not found'}), 404
+    
+    item = cur.execute('SELECT * FROM dossier WHERE id_dossier=?',[id_dossier]).fetchall()
+
+    if not item:
+        conn.close()
+        return jsonify({'success': False, 'message': 'file not found'}), 404
+
+    item = cur.execute('UPDATE dossier SET num_dossier=?, \
+            date=? WHERE id_dossier=?',[num_dossier, date, id_dossier])
+
+    conn.commit()
+    conn.close()
+    return jsonify({'success': True, 'message': "file update"}), 200
 
 
 # @app.route('/api/v1/resources/dossier/all', methods=['GET'])
